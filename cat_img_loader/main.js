b75e5dfb93f971d9aa4f43c5839c48d2954758a7
Juan Soto <jpsoto121@gmail.com>
2022-12-20 20:31:16 -0500

    favourite cats section added

--- cat_img_loader/main.js
+++ cat_img_loader/main.js
@@ -1,18 +1,71 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const API_KEY = 'live_6dp7FWHHTREaCTCSGjgr573r0nL32cHyEDrgIIC6rXLqlvSPMSeUp6dzRchqggNt';
 
-document.addEventListener('DOMContentLoaded', getCat);
+const API_URL_RANDOM = [
+  'https://api.thecatapi.com/v1/images/search',
+  '?limit=3',
+  `&api_key=${API_KEY}`,
+].join('');
 
-async function getCat() {
+const API_URL_FAVOURITES = [
+  'https://api.thecatapi.com/v1/favourites',
+  '?limit=3',
+  `&api_key=${API_KEY}`,
+].join('');
+
+const spanError = document.getElementById('error');
+
+/* document.addEventListener('DOMContentLoaded', getCats); */
+const button = document.querySelector('.cont__btn');
+
+async function getRandomCats() {
+  const res = await fetch(API_URL_RANDOM);
+  const data = await res.json();
+  
+  console.log('Random');
+  console.log(data);
+
+  if (res.status !== 200) {
+    spanError.innerText = 'An error has ocurred: ' + res.status;
+  } else {
+    const images = document.querySelectorAll('.cont__img');
+    const arrImages = [...images];
+    console.log('Array Images');
+    console.log(arrImages);
+
+    arrImages.forEach((image, index) => {
+      console.log('Images urls');
+      console.log(data[index].url);
+      image.src = data[index].url;
+    });
+  }
+}
+
+
+
+button.addEventListener('click', getRandomCats);
+
+
+async function getFavCats() {
   try {
-    const res = await fetch(URL);
+    const res = await fetch(API_URL_FAVOURITES);
     const data = await res.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const images = document.querySelectorAll('.fav__img');
+    const arrImages = [...images];
+
+    console.log('Favs');
+    console.log(data);
+    console.log(arrImages);
+    
+    arrImages.forEach((image, index) => {
+      console.log(data[index].url);
+      image.src = data[index].url;
+    });
   }
   catch (error) {
     console.log(error)
   } 
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', getCat);
+
+getRandomCats()
+getFavCats()
